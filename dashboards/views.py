5eb1c71a7d80e19a5d4b267c8324e5e78a1382da
bhavanigolagana <bhavanigolagana5@gmail.com>
2023-09-02 15:37:56 +0530

    adding blogapp to online repo

---
+++ dashboards/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from blog.models import Blog, Category
+@login_required(login_url='login')
+def dashboard(request):
+    category_count=Category.objects.all().count()
+    blogs_count=Blog.objects.all().count()
+    context={
+        'category_count':category_count,
+        'blogs_count':blogs_count,
+    }
+    return render(request,'dashboard/dashboard.html',context)
+
